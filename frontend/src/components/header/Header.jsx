767f5d0f1d0c01d76551c928dd8f6b688ca162cd
Jadline <njerijadline@gmail.com>
2025-02-18 08:37:20 -0600

    Updated UI: reduced row height to 15vh, added Logo & SearchBox components, and created the Header section.

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,17 +1,24 @@
 import styles from "./Header.module.css";
 import { useLocation } from "react-router-dom";
 import { mainPages } from "../../../public/project-data-files/MainPages";
+import Logo from "../logo/Logo";
+import SearchBox from "../searchbox/SearchBox";
 function Header({ className }) {
   const location = useLocation();
   const pageTitle = mainPages[location.pathname] || "";
   return (
     <header className={className}>
-      <p>Rolling Cargo Shipping Company</p>
-      {pageTitle && <h2>{pageTitle}</h2>}
-      <p>Search Bar</p>
-      <p>Settings</p>
-      <p>notifications</p>
-      <p>profile</p>
+
+      <Logo/>
+      {pageTitle && <p>{pageTitle}</p>}
+      
+      {/* <p>search bar</p> */}
+      
+      <SearchBox/>
+      <img role="button" src="./icons/dark.png" alt="dark-mode-icon" className={styles.darkmode}/>
+      <img role="button" src="./icons/notification.png" alt="notification-icon" className={styles.notification} />
+      <img role="button" src="./avatar/avatar.svg" alt="avatar-icon" className={styles.avatar} />
+      
     </header>
   );
 }
