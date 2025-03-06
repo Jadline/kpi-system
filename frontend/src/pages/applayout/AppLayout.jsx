fee18b4d091995f3f9697b08d70e653a1c85d2ec
Ian Gitonga <iangitonga005@gmail.com>
2025-03-06 10:42:05 +0300

    added services folder on the backend folder

--- frontend/src/pages/applayout/AppLayout.jsx
+++ frontend/src/pages/applayout/AppLayout.jsx
@@ -1,27 +1,26 @@
-import { Outlet } from "react-router-dom"
-import Header from "../../components/header/Header"
-import Sidebar from "../../components/sidebar/Sidebar"
-import InsightCard from "../../components/insight-card/InsightCard"
-import styles from './AppLayout.module.css'
-import { useState,useEffect } from "react"
-import { useUI } from "../../context/UI-Context"
+import { Outlet } from "react-router-dom";
+import Header from "../../components/header/Header";
+import Sidebar from "../../components/sidebar/Sidebar";
+import InsightCard from "../../components/insight-card/InsightCard";
+import styles from "./AppLayout.module.css";
+import { useState, useEffect } from "react";
+import { useUI } from "../../context/UI-Context";
 
-function AppLayout(){
-    const {opensidebar} = useUI()
-    
-    
-    return (
-        <div className={`${styles.appLayout}`}
-        style={{'--sidebar-width' : !opensidebar ? "26rem" : '10rem'}}
-        
-        >
-            <Header className={styles.header} />
-            <Sidebar className={styles.sidebar}/>
-            <InsightCard className={styles.card}/>
-           <main>
-            <Outlet/>
-           </main>
-        </div>
-    )
+function AppLayout() {
+  const { opensidebar } = useUI();
+
+  return (
+    <div
+      className={`${styles.appLayout}`}
+      style={{ "--sidebar-width": !opensidebar ? "26rem" : "10rem" }}
+    >
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <InsightCard className={styles.card} />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
-export default AppLayout 
+export default AppLayout;
