54b3f3d14e1d00681222f328f2e872428d44d9c1
Jadline <njerijadline@gmail.com>
2025-04-01 08:55:51 -0500

    updating changes-modified insight card to receive data dynamically

---
+++ frontend/public/project-data-files/data.js
@@ -0,0 +1,22 @@
+export const cardMappings = {
+  "/number-of-shipments": {
+    total_shipments: {
+      title: "Total Shipments",
+      type: "number",
+      icon: "/icons/total-number-of-shipments.png",
+      backgroundColor: "#FEF2D6",
+    },
+    air_shipments: {
+      title: "Shipments by Air",
+      type: "number",
+      icon: "/icons/cost-per-shipment.png",
+      backgroundColor: "#E4E4FF",
+    },
+    sea_shipments: {
+      title: "Shipments by Sea",
+      type: "number",
+      icon: "/icons/cost-per-shipment-by-sea.png",
+      backgroundColor: "#FFDED2",
+    },
+  }
+};
